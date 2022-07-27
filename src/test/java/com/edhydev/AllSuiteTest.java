85eafd98f060fc0cc30bf260ea36450e86cb3fbe
edhydev <edgar.jimenez.dev@gmail.com>
2022-07-27 09:07:42 -0500

    Tag, RunWith, SelectPackages, IncludeTags

---
+++ src/test/java/com/edhydev/AllSuiteTest.java
@@ -0,0 +1,12 @@
+package com.edhydev;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("com.edhydev")
+//@IncludeTags("database")
+public class AllSuiteTest {
+}
